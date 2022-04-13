aa55af4c54f1e82cd4d932138186ef9047fbb173
motswanax <motswanax@users.noreply.github.com>
2022-04-14 00:53:34 +0200

    Created a schema, model and saved a document to DB.

--- index.js
+++ index.js
@@ -4,3 +4,27 @@ mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+const Course = mongoose.model("Course", courseSchema); // This is a class.
+
+async function createCourse() {
+  const course = new Course({
+    name: "Angular.js",
+    author: "BAD",
+    tags: ["angular", "frontend"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+createCourse();
