d7839c79c6be23fa4edacfe5fa95ba38cb1e7a35
motswanax <motswanax@users.noreply.github.com>
2022-04-15 07:19:42 +0200

    Demo of comparison operators with MongoDB.

--- index.js
+++ index.js
@@ -28,7 +28,10 @@ async function createCourse() {
 }
 
 async function getCourses() {
-  const courses = await Course.find({ author: "BAD", isPublished: true })
+  const courses = await Course
+    //.find({ author: "BAD", isPublished: true })
+    //.find({ price: { $gt: 10, $lte: 20 } })
+    .find({ price: { $in: [10, 15, 20] } })
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
