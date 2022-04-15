cadeb5d1c0bfa5a06491344efca5ffad47cdc943
motswanax <motswanax@users.noreply.github.com>
2022-04-15 07:26:04 +0200

    Demo of logical operators and regex.

--- index.js
+++ index.js
@@ -31,7 +31,11 @@ async function getCourses() {
   const courses = await Course
     //.find({ author: "BAD", isPublished: true })
     //.find({ price: { $gt: 10, $lte: 20 } })
-    .find({ price: { $in: [10, 15, 20] } })
+    //.find({ price: { $in: [10, 15, 20] } })
+    //.find()
+    //.or([{ author: "BAD" }, { isPublished: true }])
+    //.and([])
+    .find({author: /^BAD/}) // Regex
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
